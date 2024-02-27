0b9f62dfa896cca3d35a2c762000836c58e5e6f9
송윤호 <131519351+sdbsgh98@users.noreply.github.com>
2024-02-27 09:40:38 +0900

    [Bronze I] Title: 단어 공부, Time: 536 ms, Memory: 32020 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String str = sc.nextLine().toUpperCase();
+            sc.close();
+
+            int[] arr = new int[26];
+            int max = 0;
+            char result = 0;
+
+            for (int i = 0; i < str.length(); i++) {
+                arr[str.charAt(i) - 65]++;
+
+                if (max < arr[str.charAt(i) - 65]) {
+                    max = arr[str.charAt(i) - 65];
+                    result = str.charAt(i);
+
+                } else if (max == arr[str.charAt(i) - 65]) {
+                    result = '?';
+                }
+            }
+            System.out.println(result);
+    }
+}
